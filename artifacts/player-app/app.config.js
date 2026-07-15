95534b48bf8ae61b95d1fdc3cee6e49e1dbf9f23
fbelorp-alt <fbelo.rp@gmail.com>
2026-07-15 16:00:25 -0300

    v1.14.89 versionCode 107

--- artifacts/player-app/app.config.js
+++ artifacts/player-app/app.config.js
@@ -14,7 +14,7 @@ const config = {
   name: "RPSHOW TV",
   slug: "player-app",
   owner: "rpshow-vnnox-on",
-  version: "1.14.88",
+  version: "1.14.89",
   orientation: "landscape",
   icon: "./assets/images/icon.png",
   scheme: "rpshow-player",
@@ -33,7 +33,7 @@ const config = {
   },
   android: {
     package: "com.rpshow.signageplayer",
-    versionCode: 106,
+    versionCode: 107,
     usesCleartextTraffic: true,
     adaptiveIcon: {
       foregroundImage: "./assets/images/icon.png",
