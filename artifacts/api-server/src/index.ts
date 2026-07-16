0f5f00e35765a3ebb850a39c63e7324b8e888e48
fbelorp-alt <fbelo.rp@gmail.com>
2026-07-16 02:57:54 -0300

    sync: index.ts

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 import { logger } from "./lib/logger";
 import { startOfflineMonitor } from "./lib/offlineMonitor";
+import { startCampaignEndNotifier } from "./lib/campaignEndNotifier";
 
 const rawPort = process.env["PORT"];
 
@@ -24,4 +25,5 @@ app.listen(port, (err) => {
 
   logger.info({ port }, "Server listening");
   startOfflineMonitor();
+  startCampaignEndNotifier();
 });
