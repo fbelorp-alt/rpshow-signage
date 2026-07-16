e67fc41f25387d7f3f8158efccf69dedd3e59a44
fbelorp-alt <fbelo.rp@gmail.com>
2026-07-16 03:00:13 -0300

    sync: lib/api-zod/dist/index.d.ts

---
+++ lib/api-zod/dist/index.d.ts
@@ -0,0 +1,12 @@
+export * from "./generated/api";
+export * from "./generated/types";
+export { ReorderPlaylistItemsBody } from "./generated/api";
+export { UpdatePlaylistItemBody } from "./generated/api";
+export { UpdateMediaBody } from "./generated/api";
+export { AssignScreenToGroupBody } from "./generated/api";
+export { CreateEmergencyAlertBody } from "./generated/api";
+export { CreateScreenGroupBody } from "./generated/api";
+export { PushPlaylistToGroupBody } from "./generated/api";
+export { UnassignScreenFromGroupBody } from "./generated/api";
+export { UpdateScreenGroupBody } from "./generated/api";
+//# sourceMappingURL=index.d.ts.map
