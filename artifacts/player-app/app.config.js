0f1b7098d1f1229b9b4eb088f6471d9a6e1b98f6
fbelorp-alt <fbelo.rp@gmail.com>
2026-07-18 00:23:55 -0300

    v1.15.14 (134): splash restaurado com logo.png

--- artifacts/player-app/app.config.js
+++ artifacts/player-app/app.config.js
@@ -16,7 +16,7 @@ const config = {
   name: "RPSHOW TV",
   slug: "player-app",
   owner: "rpshow-vnnox-on",
-  version: "1.15.13",
+  version: "1.15.14",
   orientation: "landscape",
   icon: "./assets/images/icon.png",
   scheme: "rpshow-player",
@@ -35,7 +35,7 @@ const config = {
   },
   android: {
     package: "com.rpshow.signageplayer",
-    versionCode: 133,
+    versionCode: 134,
     usesCleartextTraffic: true,
     adaptiveIcon: {
       foregroundImage: "./assets/images/adaptive-icon.png",
