0bb8397178fd3350d1eac4b9860a950d11561b80
fbelorp-alt <fbelo.rp@gmail.com>
2026-07-23 22:56:15 -0300

    chore(player): bump version 1.15.38 / versionCode 165

--- artifacts/player-app/app.config.js
+++ artifacts/player-app/app.config.js
@@ -16,7 +16,7 @@ const config = {
   name: "RPSHOW TV",
   slug: "player-app",
   owner: "rpshow-vnnox-on",
-  version: "1.15.37",
+  version: "1.15.38",
   orientation: "landscape",
   icon: "./assets/images/icon.png",
   scheme: "rpshow-player",
@@ -35,7 +35,7 @@ const config = {
   },
   android: {
     package: "com.rpshow.signageplayer",
-    versionCode: 164,
+    versionCode: 165,
     usesCleartextTraffic: true,
     adaptiveIcon: {
       foregroundImage: "./assets/images/adaptive-icon.png",
